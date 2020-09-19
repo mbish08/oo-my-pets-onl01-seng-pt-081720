10d83beb8d97e3fc9d15955a6055ce73762897b7
Mikel Bishop <mdrew8@gmail.com>
2020-09-19 21:40:57 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -62,7 +62,7 @@ class Owner
   def sell_pets
     self.dogs.each { |dog| dog.mood = "nervous" }
     self.cats.each { |cat| cat.mood = "nervous" }
-    self.reset_all
+    Owner.reset_all
   end 
   
 end
